bb0d037a7c0e3dd385bef5200bb2f4d9a64fa3d4
Gabor <pergarbor@gmail.com>
2013-11-30 17:01:44 +0100

    Base logic for UDP proxy (not tested)

---
+++ src/main/java/pl/edu/agh/dropper/proxy/UDPProxy.java
@@ -0,0 +1,57 @@
+package pl.edu.agh.dropper.proxy;
+
+
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.ArrayList;
+
+/**
+ * Klasa sluzaca do zarzadzania serwerem proxy
+ */
+public class UDPProxy {
+    private ArrayList<UDPProxyThread> threads;
+    
+    public UDPProxy() {
+        threads = new ArrayList<UDPProxyThread>();        
+    }
+    
+    /**
+     * Dodaje mapowanie z lokalnego portu na zdalny adres/port. Uruchamia watek, ktory odbiera pakiety przychodzace
+     * na lokalny port i przesyla je dalej na adres/port zdalny.
+     * @param localPort
+     * @param remoteAddress
+     * @param remotePort
+     * @throws SocketException
+     */
+    public void addProxyMapping(int localPort, InetAddress remoteAddress, int remotePort) throws SocketException {
+        DatagramSocket ds = new DatagramSocket(localPort);
+        UDPProxyThread thread = new UDPProxyThread(this, ds, remoteAddress, remotePort);
+        thread.start();
+        threads.add(thread);
+    }
+
+    /**
+     * Zatrzymuje wszystkie watki
+     */
+    public void stopServer() {
+        for(UDPProxyThread thread : threads) {
+            thread.finish();
+        }
+    }
+
+    /**
+     * Ustawia procent pakietow, ktore maja byc zablokowane
+     */
+    public void setDropRate() {
+        //TODO
+    }
+
+    /**
+     * Ustawia maksymalna przepustowosc serwera proxy
+     */
+    public void setBandwidth() {
+        //TODO
+    }
+
+}
