334140ce1f45ef82af676d84770c1756e58deda3
fracz <wojciech@fracz.pl>
2013-12-08 20:50:02 +0100

    Implement changing manipulator

--- src/main/java/pl/edu/agh/dropper/manipulator/ChangingManipulator.java
+++ src/main/java/pl/edu/agh/dropper/manipulator/ChangingManipulator.java
@@ -2,17 +2,27 @@ package pl.edu.agh.dropper.manipulator;
 
 import pl.edu.agh.dropper.proxy.Packet;
 
+import java.util.Random;
+
 /**
  * Packet manipulator that changes the packet content.
  */
 public class ChangingManipulator extends AbstractPacketManipulator {
+    private final Random random = new Random(System.currentTimeMillis());
+
     public ChangingManipulator(double probability) {
         super(probability);
     }
 
     @Override
     protected Packet doManipulation(Packet packet) {
-        // TODO implement
-        return packet;
+        byte[] data = packet.getData();
+        int bytesToChange = random.nextInt(data.length);
+        byte[] bytes = new byte[bytesToChange];
+        random.nextBytes(bytes);
+        for (byte b : bytes)
+            data[random.nextInt(data.length)] = b;
+        Packet modified = new Packet(data);
+        return modified;
     }
 }
